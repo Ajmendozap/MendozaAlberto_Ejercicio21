ee574357692fb9078f64c9aa6fe15263d4c5924e
Default user <aj.mendoza@uniandes.edu.co>
2019-10-22 18:38:37 +0000

    Código y gráfica del coseno

--- tarea.py
+++ tarea.py
@@ -1,9 +1,9 @@
 import numpy as np
 import matplotlib.pylab as plt
-x= np.linspace(-2*np.pi,2*np.pi,10)
-y= np.sin(x)
-plt.ylabel('sen(x)')
+x= np.linspace(-2*np.pi,2*np.pi,100)
+y= np.cos(x)
+plt.ylabel('cos(x)')
 plt.xlabel('x')
 plt.plot(x,y)
-plt.savefig('seno.png')
+plt.savefig('coseno.png')
 plt.show()
